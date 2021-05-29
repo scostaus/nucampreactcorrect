fe16f16067c8a30912cc34d7c2e66f7cbe2caee7
Sean Costanza <seancostanza@Seans-MacBook-Pro-2.local>
2021-05-28 21:12:59 -0500

    React Redux Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -14,7 +14,7 @@ const mapStateToProps = state => {
       campsites: state.campsites,
       comments: state.comments,
       partners: state.partners,
-      promotions: state.promotion,
+      promotions: state.promotions
   };
 };
 
@@ -22,40 +22,38 @@ class Main extends Component {
   
   render() {
       
-      const HomePage = () => {
-        return (
-          <Home 
+    const HomePage = () => {
+      return (
+          <Home
               campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
               promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
               partner={this.props.partners.filter(partner => partner.featured)[0]}
           />
-        );
-      }
-
-      const CampsiteWithId = ({match}) => {
-        return(
-          <CampsiteInfo 
-            campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
-            />
-        );
-      }
-
-      return (
-          <div>
-              <Header />
-              <Switch>
-                <Route path='/home' component={HomePage} />
-                <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                <Route exact path='/contactus' component={Contact} />
-                <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
-                <Redirect to='/home' />
-              </Switch>
-              <Footer />
-          </div>
       );
   };
+
+  const CampsiteWithId = ({match}) => {
+      return (
+          <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+      );
+  };
+
+  return (
+      <div>
+          <Header />
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+              <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+              <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
+              <Redirect to='/home' />
+          </Switch>
+          <Footer />
+      </div>
+  );
+}
 }
 
 export default withRouter(connect(mapStateToProps)(Main));
